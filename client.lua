240c223c6328a70b370ab5a4c7bb18c9aa48b8fa
WasabiRobby <79250053+WasabiRobby@users.noreply.github.com>
2022-04-21 11:46:34 -0700

    fix: Relogging and peds not spawning
    Added Player variables to within the loop.

--- client.lua
+++ client.lua
@@ -92,8 +92,8 @@ end)
 
 -- Starting NPC Spawn
 CreateThread(function()
-    local Player = PlayerPedId()
     while true do
+        local Player = PlayerPedId()
         for k, v in pairs(Config.NPCLocations) do
             local Pos = GetEntityCoords(Player)
             local Dist = #(Pos - vector3(v.Coords[1],v.Coords[2],v.Coords[3]))
@@ -126,8 +126,8 @@ end)
 -- Events
 
 CreateThread(function()
-    local Player = PlayerPedId()
     while true do
+        local Player = PlayerPedId()
         local Sleep = 1500
         local Pos = GetEntityCoords(Player)
         for k, v in pairs(Config.NPCLocations) do
